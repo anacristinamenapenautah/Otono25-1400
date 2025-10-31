1727d23772f1325928834bd0a8095bfb5f83acfe
anacristinamenapenautah <anamenapena@mail.weber.edu>
2025-10-30 18:56:27 -0600

    Clases 10302025

---
+++ M10_Presencial/clases.py
@@ -0,0 +1,12 @@
+def area_rectangulo(largo, ancho):
+    return largo*ancho
+class gato:
+    def _init_(self, nombre, edad):
+        self.nombre = nombre
+        self.edad = edad
+        
+        def maullar(self):
+            return "Miai!"
+        
+tu_gato = gato ("Luna",2)
+print (tu_gato.maullar())
